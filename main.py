3bf168e7b117d04d28152593efb058873c03d8eb
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-04-07 13:00:12 +0200

    eerste resize poging

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ gray = cv2.cvtColor( image, cv2.COLOR_BGR2GRAY)
 #cv2.imshow('Original image', image)
 #cv2.imshow('Gray boi', gray)
 
+dim = (48, 48)
+resized = cv2.resize(gray, dim, interpolation = cv2.INTER_AREA)
+
 cv2.imwrite("original.jpg",image)
 cv2.imwrite("gray.jpg", gray) 
 
