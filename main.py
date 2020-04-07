f0c592259f6991d22764663b5edee2496034e1b5
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-04-07 14:24:18 +0200

    test met INTER_lINEAR

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ gray = cv2.cvtColor( image, cv2.COLOR_BGR2GRAY)
 #cv2.imshow('Gray boi', gray)
 
 dim = (48, 48)
-resized = cv2.resize(gray, dim, interpolation = cv2.INTER_AREA)
+resized = cv2.resize(gray, dim, interpolation = cv2.INTER_LINEAR)
 
 #opslaan van de afbeeldingen voor evaluatie
 cv2.imwrite("original.jpg",image)
@@ -23,7 +23,13 @@ cv2.imwrite("gray.jpg", resized)
 # cv2.destroyAllWindows()
 
 """
-#time it cost to run 1 image = 1.96884473085
+#time it cost to run 1 image = 1.96884473085 
+
+#| method of resizing | method of grayscaling | speed |
+#| INTER_AREA | COLOR_BGR2GRAY | 1.96884473085 |
+#| INTER_LINEAR | COLOR_BGR2GRAY | 
+
+
 #f187ff244ecfc9e264b67ae04aca4498fe608f6b van gray image 
 elapsed_time = timeit.timeit(codeToTest, number=100)/100
 print(elapsed_time)
