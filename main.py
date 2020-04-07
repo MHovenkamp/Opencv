1bc5e3c33562ea403ac654c3f000d70dd8c51b8c
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-04-07 12:55:15 +0200

    afbeelding

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import cv2 #to read the image
 
-image = cv2.imread("~/Opencv/images/jared.jpg")
+image = cv2.imread('/home/pi/Opencv/images/bigJared.jpg')
 gray = cv2.cvtColor( image, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow('Original image', image)
-cv2.imshow('Gray boi', gray)
+#cv2.imshow('Original image', image)
+#cv2.imshow('Gray boi', gray)
+
+cv2.imwrite("original.jpg",image)
+cv2.imwrite("gray.jpg", gray) 
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
