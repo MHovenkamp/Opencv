b6822b6cb6376956bf302d626afc12b677219450
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-04-07 16:00:58 +0200

    test met INTER_NEAREST

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ dim = (48, 48)
 #resized = cv2.resize(gray, dim, interpolation = cv2.INTER_AREA)
 #resized = cv2.resize(gray, dim, interpolation = cv2.INTER_LINEAR)
 #resized = cv2.resize(gray, dim, interpolation = cv2.INTER_CUBIC)
-resized = cv2.resize(gray, dim, interpolation = cv2.INTER_LANCZOS4)
-#resized = cv2.resize(gray, dim, interpolation = cv2.INTER_NEAREST)
+#resized = cv2.resize(gray, dim, interpolation = cv2.INTER_LANCZOS4)
+resized = cv2.resize(gray, dim, interpolation = cv2.INTER_NEAREST)
 
 #opslaan van de afbeeldingen voor evaluatie
 cv2.imwrite("original.jpg",image)
@@ -33,11 +33,11 @@ cv2.imwrite("gray.jpg", resized)
 # Dit zijn de testen op 1 afbeelding aangezien ik nog geen database heb
 # Volgorde grayscalling -> Resizing
 #| method of resizing | method of grayscaling | speed | shasum |
-#| INTER_AREA | COLOR_BGR2GRAY | 1.96884473085 | #f187ff244ecfc9e264b67ae04aca4498fe608f6b |
-#| INTER_LINEAR | COLOR_BGR2GRAY | 1.6719028306 | 3cbf05488ee00e09193cf213c9db1c8ff3f9dd05 |
-#| INTER_CUBIC | COLOR_BGR2GRAY | 1.63356585026 | 8cc27a0f5c6bb12fca6dbcd73c742d706f8be9e0 |
-#| INTER_LANCZOS4 | COLOR_BGR2GRAY | 
-#| INTER_NEAREST | COLOR_BGR2GRAY | 
+#| INTER_AREA | COLOR_BGR2GRAY | +-1.96884473085 | #f187ff244ecfc9e264b67ae04aca4498fe608f6b |
+#| INTER_LINEAR | COLOR_BGR2GRAY | +-1.6719028306 | 3cbf05488ee00e09193cf213c9db1c8ff3f9dd05 |
+#| INTER_CUBIC | COLOR_BGR2GRAY | +-1.63356585026 | 8cc27a0f5c6bb12fca6dbcd73c742d706f8be9e0 |
+#| INTER_LANCZOS4 | COLOR_BGR2GRAY | +- 1.62078979969 | 24ebc32e9d596af09c3043905858c09717157406 | 
+#| INTER_NEAREST | COLOR_BGR2GRAY | +-
 
 
 
