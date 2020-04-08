57373aee354c7afd0a9037590a79ebfdef329abe
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-04-08 10:14:31 +0200

    nieuwe manier van images doorpompen

--- main.py
+++ main.py
@@ -1,50 +1,51 @@
 import cv2 #to read the image
 import time
+import os
 
 import cv2
 
 path = "/home/pi/Opencv/images/"
-name = raw_input()
-image = cv2.imread(path + name)
-tStart = time.time()
-gray = cv2.cvtColor( image, cv2.COLOR_BGR2GRAY)
+for file in os.listdir(path):
+    image = cv2.imread(path + file)
+    tStart = time.time()
+    gray = cv2.cvtColor( image, cv2.COLOR_BGR2GRAY)
 
 
-#laten zien van de images, staat uit voor terminal gebruik
-#cv2.imshow('Original image', image)
-#cv2.imshow('Gray boi', gray)
+    #laten zien van de images, staat uit voor terminal gebruik
+    #cv2.imshow('Original image', image)
+    #cv2.imshow('Gray boi', gray)
 
-dim = (48, 48)
-resized = cv2.resize(gray, dim, interpolation = cv2.INTER_AREA)
-#resized = cv2.resize(gray, dim, interpolation = cv2.INTER_LINEAR)
-#resized = cv2.resize(gray, dim, interpolation = cv2.INTER_CUBIC)
-#resized = cv2.resize(gray, dim, interpolation = cv2.INTER_LANCZOS4)
-#resized = cv2.resize(gray, dim, interpolation = cv2.INTER_NEAREST)
+    dim = (48, 48)
+    resized = cv2.resize(gray, dim, interpolation = cv2.INTER_AREA)
+    #resized = cv2.resize(gray, dim, interpolation = cv2.INTER_LINEAR)
+    #resized = cv2.resize(gray, dim, interpolation = cv2.INTER_CUBIC)
+    #resized = cv2.resize(gray, dim, interpolation = cv2.INTER_LANCZOS4)
+    #resized = cv2.resize(gray, dim, interpolation = cv2.INTER_NEAREST)
 
-#opslaan van de afbeeldingen voor evaluatie
-cv2.imwrite("original.jpg",image)
-cv2.imwrite("gray.jpg", resized) 
+    #opslaan van de afbeeldingen voor evaluatie
+    cv2.imwrite("original.jpg",image)
+    cv2.imwrite("gray.jpg", resized) 
 
-# Voor laten zien afbeeldingen, uit vo rterminal gebruik
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+    # Voor laten zien afbeeldingen, uit vo rterminal gebruik
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
-tEnd = time.time()
+    tEnd = time.time()
 
-#time it cost to run 1 image = 1.96884473085 
+    #time it cost to run 1 image = 1.96884473085 
 
-# Dit zijn de testen op 1 afbeelding aangezien ik nog geen database heb
-# Volgorde grayscalling -> Resizing
-#| method of resizing | method of grayscaling | speed | shasum |
-#| INTER_AREA | COLOR_BGR2GRAY | +-1.96884473085 | #f187ff244ecfc9e264b67ae04aca4498fe608f6b |
-#| INTER_LINEAR | COLOR_BGR2GRAY | +-1.6719028306 | 3cbf05488ee00e09193cf213c9db1c8ff3f9dd05 |
-#| INTER_CUBIC | COLOR_BGR2GRAY | +-1.63356585026 | 8cc27a0f5c6bb12fca6dbcd73c742d706f8be9e0 |
-#| INTER_LANCZOS4 | COLOR_BGR2GRAY | +- 1.62078979969 | 24ebc32e9d596af09c3043905858c09717157406 | 
-#| INTER_NEAREST | COLOR_BGR2GRAY | +-
+    # Dit zijn de testen op 1 afbeelding aangezien ik nog geen database heb
+    # Volgorde grayscalling -> Resizing
+    #| method of resizing | method of grayscaling | speed | shasum |
+    #| INTER_AREA | COLOR_BGR2GRAY | +-1.96884473085 | #f187ff244ecfc9e264b67ae04aca4498fe608f6b |
+    #| INTER_LINEAR | COLOR_BGR2GRAY | +-1.6719028306 | 3cbf05488ee00e09193cf213c9db1c8ff3f9dd05 |
+    #| INTER_CUBIC | COLOR_BGR2GRAY | +-1.63356585026 | 8cc27a0f5c6bb12fca6dbcd73c742d706f8be9e0 |
+    #| INTER_LANCZOS4 | COLOR_BGR2GRAY | +- 1.62078979969 | 24ebc32e9d596af09c3043905858c09717157406 | 
+    #| INTER_NEAREST | COLOR_BGR2GRAY | +-
 
 
-elapsedTime =  tEnd-tStart
-print(elapsedTime)
+    elapsedTime =  tEnd-tStart
+    print(elapsedTime)
 
-results = open('results.txt', "a")
-results.write( name + ' ' + str(elapsedTime) + '\n' )
+    results = open('results.txt', "a")
+    results.write( file + ' ' + str(elapsedTime) + '\n' )
