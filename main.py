9d8309f901093b98e664cc263c4c7ef7c1917c90
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-04-08 11:24:46 +0200

    eerste run met volledig systeem

--- main.py
+++ main.py
@@ -6,57 +6,57 @@ import cv2
 
 resultsList = []
 
-results = open('/home/pi/Opencv/Results/results.txt', "a")
-path = "/home/pi/Opencv/Images/"
-pathResults = "/home/pi/Opencv/Results/"
-for file in os.listdir(path):
-    image = cv2.imread(path + file)
-    tStart = time.time()
-    gray = cv2.cvtColor( image, cv2.COLOR_BGR2GRAY)
+paths = [   "/home/pi/Opencv/Results/Area/", 
+            "/home/pi/Opencv/Results/Cubic/", 
+            "/home/pi/Opencv/Results/Lanczos4/", 
+            "/home/pi/Opencv/Results/Linear/", 
+            "/home/pi/Opencv/Results/nearest/"]
+methods = [ cv2.INTER_AREA,
+            cv2.INTER_CUBIC,
+            cv2.INTER_LANCZOS4,
+            cv2.INTER_LINEAR,
+            cv2.INTER_NEAREST]
+resultFiles = ['/home/pi/Opencv/Results/resultsArea.txt',
+                '/home/pi/Opencv/Results/resultsCubic.txt',
+                '/home/pi/Opencv/Results/resultsLanczos4.txt',
+                '/home/pi/Opencv/Results/resultsLinear.txt',
+                '/home/pi/Opencv/Results/resultsNearest.txt']
+for i in range(len(methods)):
+    results = open(resultFiles[i], "a")
+    path = "/home/pi/Opencv/Images/"
+    pathResults = paths[i]
+
+    for file in os.listdir(path):
+        image = cv2.imread(path + file)
+        tStart = time.time()
+        gray = cv2.cvtColor( image, cv2.COLOR_BGR2GRAY)
 
 
-    #laten zien van de images, staat uit voor terminal gebruik
-    #cv2.imshow('Original image', image)
-    #cv2.imshow('Gray boi', gray)
+        #laten zien van de images, staat uit voor terminal gebruik
+        #cv2.imshow('Original image', image)
+        #cv2.imshow('Gray boi', gray)
 
-    dim = (48, 48)
-    resized = cv2.resize(gray, dim, interpolation = cv2.INTER_AREA)
-    #resized = cv2.resize(gray, dim, interpolation = cv2.INTER_LINEAR)
-    #resized = cv2.resize(gray, dim, interpolation = cv2.INTER_CUBIC)
-    #resized = cv2.resize(gray, dim, interpolation = cv2.INTER_LANCZOS4)
-    #resized = cv2.resize(gray, dim, interpolation = cv2.INTER_NEAREST)
+        dim = (48, 48)
+        resized = cv2.resize(gray, dim, interpolation = methods[i])
 
-    #opslaan van de afbeeldingen voor evaluatie
+        #opslaan van de afbeeldingen voor evaluatie
+        grayName = pathResults + file + "Gray.jpg"
+        resizedName = pathResults + file + "Gray_resized.jpg"
 
-    grayName = pathResults + file + "Gray.jpg"
-    resizedName = pathResults + file + "Gray_resized.jpg"
+        cv2.imwrite(grayName,image)
+        cv2.imwrite(resizedName, resized) 
 
-    cv2.imwrite(grayName,image)
-    cv2.imwrite(resizedName, resized) 
+        # Voor laten zien afbeeldingen, uit vo rterminal gebruik
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
 
-    # Voor laten zien afbeeldingen, uit vo rterminal gebruik
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+        tEnd = time.time()
 
-    tEnd = time.time()
+        elapsedTime =  tEnd-tStart
+        resultsList.append(elapsedTime)
+        print(elapsedTime)
 
-    #time it cost to run 1 image = 1.96884473085 
+        results.write( file + ': ' + str(elapsedTime) + '\n' )
 
-    # Dit zijn de testen op 1 afbeelding aangezien ik nog geen database heb
-    # Volgorde grayscalling -> Resizing
-    #| method of resizing | method of grayscaling | speed | shasum |
-    #| INTER_AREA | COLOR_BGR2GRAY | +-1.96884473085 | #f187ff244ecfc9e264b67ae04aca4498fe608f6b |
-    #| INTER_LINEAR | COLOR_BGR2GRAY | +-1.6719028306 | 3cbf05488ee00e09193cf213c9db1c8ff3f9dd05 |
-    #| INTER_CUBIC | COLOR_BGR2GRAY | +-1.63356585026 | 8cc27a0f5c6bb12fca6dbcd73c742d706f8be9e0 |
-    #| INTER_LANCZOS4 | COLOR_BGR2GRAY | +- 1.62078979969 | 24ebc32e9d596af09c3043905858c09717157406 | 
-    #| INTER_NEAREST | COLOR_BGR2GRAY | +-
-
-
-    elapsedTime =  tEnd-tStart
-    resultsList.append(elapsedTime)
-    print(elapsedTime)
-
-    results.write( file + ': ' + str(elapsedTime) + '\n' )
-
-average = sum(resultsList) / len(resultsList)
-results.write("Average: " + str(average))
+    average = sum(resultsList) / len(resultsList)
+    results.write("Average: " + str(average))
