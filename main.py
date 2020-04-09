f78dcf6b2a18e0eafdcf665894fac5a614ca919a
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-04-09 12:25:31 +0200

    herstellen code na implementeren van methoden die niet bestaan

--- main.py
+++ main.py
@@ -10,23 +10,17 @@ paths = [   "/home/pi/Opencv/Results/Area/",
             "/home/pi/Opencv/Results/Cubic/", 
             "/home/pi/Opencv/Results/Lanczos4/", 
             "/home/pi/Opencv/Results/Linear/", 
-            "/home/pi/Opencv/Results/Nearest/",
-            "/home/pi/Opencv/Results/Linear_exact/",
-            "/home/pi/Opencv/Results/Max/"]
+            "/home/pi/Opencv/Results/Nearest/"]
 methods = [ cv2.INTER_AREA,
             cv2.INTER_CUBIC,
             cv2.INTER_LANCZOS4,
             cv2.INTER_LINEAR,
-            cv2.INTER_NEAREST,
-            INTER_LINEAR_EXACT,
-            INTER_MAX]
+            cv2.INTER_NEAREST]
 resultFiles = ['/home/pi/Opencv/Results/resultsArea.txt',
                 '/home/pi/Opencv/Results/resultsCubic.txt',
                 '/home/pi/Opencv/Results/resultsLanczos4.txt',
                 '/home/pi/Opencv/Results/resultsLinear.txt',
-                '/home/pi/Opencv/Results/resultsNearest.txt',
-                '/home/pi/Opencv/Results/Linear_exact.txt',
-                '/home/pi/Opencv/Results/max.txt']
+                '/home/pi/Opencv/Results/resultsNearest.txt']
 for i in range(len(methods)):
     total = open('/home/pi/Opencv/Results/total.txt', "a")
     results = open(resultFiles[i], "a")
