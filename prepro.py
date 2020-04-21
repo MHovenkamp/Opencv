8495e1e0aab1e0483057ea45620e749400bdadd7
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-04-21 11:29:38 +0200

    test 1 duurde raar lang en heb onderbroken

--- prepro.py
+++ prepro.py
@@ -3,17 +3,17 @@ import time
 import os
 
 path = "/home/pi/Opencv/Images/FACES/"
-face_cascade = cv2.CascadeClassifier("home/pi/opencv/data/haarcascades/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier("home/pi/opencv/data/haarcascades/haarcascade_frontalface_default.xml")
 for file in os.listdir(path):
     image = cv2.imread( path + file )
     imageRGB = cv2.cvtColor( image, cv2.COLOR_BGR2RGB)
     imageCopy = imageRGB.copy()
     imageGray = cv2.cvtColor( imageRGB, cv2.COLOR_RGB2GRAY )
+    cv2.imwrite( "og.png", imageGray )
+    #face = face_cascade.detectMultiScale( imageGray, 1.25, 6)
 
-    face = face_cascade.detectMultiScale( imageGray, 1.25, 6)
-
-    x, y, w, h = [v for v in face ]
-    cv2.rectangle(imageCopy, (x,y), (x+w, y+h), (255,0,0), 3)
-    faceCrop = gray_image[y:y+h, x:x+w]
+    #x, y, w, h = [v for v in face ]
+    #cv2.rectangle(imageCopy, (x,y), (x+w, y+h), (255,0,0), 3)
+    #faceCrop = gray_image[y:y+h, x:x+w]
 
 cv2.imwrite( "result.png", faceCrop )
