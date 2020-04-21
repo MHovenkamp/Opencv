8093cab241f7f8a23f7641f7804504676ec912fc
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-04-21 11:23:31 +0200

    toevoegen cascade

--- prepro.py
+++ prepro.py
@@ -3,6 +3,7 @@ import time
 import os
 
 path = "/home/pi/Opencv/Images/FACES/"
+face_cascade = cv2.CascadeClassifier("home/pi/opencv/data/haarcascades/haarcascade_frontalface_default.xml')
 for file in os.listdir(path):
     image = cv2.imread( path + file )
     imageRGB = cv2.cvtColor( image, cv2.COLOR_BGR2RGB)
