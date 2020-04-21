2443671cc12aba341ab0355eaa7a1962394e5e2a
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-04-21 13:41:12 +0200

    aanpassingen tegen overschrijvingen

--- prepro.py
+++ prepro.py
@@ -9,11 +9,13 @@ for file in os.listdir(path):
     imageRGB = cv2.cvtColor( image, cv2.COLOR_BGR2RGB)
     imageCopy = imageRGB.copy()
     imageGray = cv2.cvtColor( imageRGB, cv2.COLOR_RGB2GRAY )
-    cv2.imwrite( "og.png", imageGray )
-    #face = face_cascade.detectMultiScale( imageGray, 1.25, 6)
+    print( file )
+    cv2.imwrite( file + ".png", imageGray )
+    face = face_cascade.detectMultiScale( imageGray, 1.25, 6)
 
     #x, y, w, h = [v for v in face ]
     #cv2.rectangle(imageCopy, (x,y), (x+w, y+h), (255,0,0), 3)
     #faceCrop = gray_image[y:y+h, x:x+w]
 
 cv2.imwrite( "result.png", faceCrop )
+ 
