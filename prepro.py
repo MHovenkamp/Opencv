a2fbdaf8bffd8e25df1172fa5fa362d047a8c83a
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-04-21 14:25:53 +0200

    eerste volledige resultaten

--- prepro.py
+++ prepro.py
@@ -3,6 +3,7 @@ import time
 import os
 
 path = "/home/pi/Opencv/Images/FACES/"
+resultPath = "/home/pi/Opencv/CropResult/"
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 for file in os.listdir(path):
     image = cv2.imread( path + file )
@@ -10,12 +11,12 @@ for file in os.listdir(path):
     imageCopy = imageRGB.copy()
     imageGray = cv2.cvtColor( imageRGB, cv2.COLOR_RGB2GRAY )
     print( file )
-    cv2.imwrite( file + ".png", imageGray )
+    #cv2.imwrite( file + ".png", imageGray )
     face = face_cascade.detectMultiScale( imageGray, 1.25, 6)
     for f in face:
         x, y, w, h = [v for v in f ]
         cv2.rectangle(imageCopy, (x,y), (x+w, y+h), (255,0,0), 3)
         faceCrop = imageGray[y:y+h, x:x+w]
-    resized = cv2.resize(faceCrop, dim, interpolation = INTER_LINEAR)
-    cv2.imwrite( file + "result.png", faceCrop )
+    resized = cv2.resize(faceCrop, (64,64), interpolation = cv2.INTER_LINEAR)
+    cv2.imwrite( resultPath + file + "result.png", faceCrop )
 
