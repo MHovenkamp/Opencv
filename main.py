9a77116d3b288597163faca2da6923ddb114c609
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-04-23 15:24:18 +0200

    schoon voo rtest met opslaan

--- main.py
+++ main.py
@@ -51,10 +51,10 @@ for i in range(len(methods)):
         resized = cv2.resize(faceCrop, dim, interpolation = methods[i])
 
         #opslaan van de afbeeldingen
-#        grayName = pathResults + file + "Gray.png"
-#        resizedName = pathResults + file + "Gray_resized.png"
-#        cv2.imwrite(grayName,gray)
-#        cv2.imwrite(resizedName, resized) 
+       grayName = pathResults + file + "Gray.png"
+       resizedName = pathResults + file + "Gray_resized.png"
+       cv2.imwrite(grayName,gray)
+       cv2.imwrite(resizedName, resized) 
 
         tEnd = time.time()
 
