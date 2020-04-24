cb26ac44bc7055883d391e698f26945b353de08c
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-04-24 09:27:07 +0200

    test 200x200 zonder opslaan schoonmaak

--- main.py
+++ main.py
@@ -20,49 +20,50 @@ resultFiles = ['/home/pi/Opencv/Results/resultsArea.txt',
                 '/home/pi/Opencv/Results/resultsLanczos4.txt',
                 '/home/pi/Opencv/Results/resultsLinear.txt',
                 '/home/pi/Opencv/Results/resultsNearest.txt']
-for i in range(len(methods)):
-    total = open('/home/pi/Opencv/Results/total.txt', "a")
-    results = open(resultFiles[i], "a")
-    path = "/home/pi/Opencv/Images/FACES/"
-    pathResults = paths[i]
+for i in range(10):
+    for i in range(len(methods)):
+        total = open('/home/pi/Opencv/Results/total.txt', "a")
+        results = open(resultFiles[i], "a")
+        path = "/home/pi/Opencv/Images/200x200/"
+        pathResults = paths[i]
 
-    for file in os.listdir(path):
-        image = cv2.imread(path + file)
-        tStart = time.time()
-        
-        #grayscalling
-	print( path+file )
-        image = cv2.imread( path + file )
-        imageRGB = cv2.cvtColor( image, cv2.COLOR_BGR2RGB)
-        imageCopy = imageRGB.copy()
+        for file in os.listdir(path):
+            image = cv2.imread(path + file)
+            tStart = time.time()
+            
+            #grayscalling
+        print( path+file )
+            image = cv2.imread( path + file )
+            imageRGB = cv2.cvtColor( image, cv2.COLOR_BGR2RGB)
+            imageCopy = imageRGB.copy()
 
-        imageGray = cv2.cvtColor( imageRGB, cv2.COLOR_RGB2GRAY )
-        #laten zien van de images, staat uit voor terminal gebruik
-        #cv2.imshow('Original image', image)
-        #cv2.imshow('Gray boi', gray)
-        face = faceCascade.detectMultiScale( imageGray, 1.25, 6)
-        for f in face:
-            x, y, w, h = [v for v in f ]
-            cv2.rectangle(imageCopy, (x,y), (x+w, y+h), (255,0,0), 3)
-            #croppen van gezicht
-            faceCrop = imageGray[y:y+h, x:x+w]
-        #resizen
-        dim = (64, 64)
-        resized = cv2.resize(faceCrop, dim, interpolation = methods[i])
+            imageGray = cv2.cvtColor( imageRGB, cv2.COLOR_RGB2GRAY )
+            #laten zien van de images, staat uit voor terminal gebruik
+            #cv2.imshow('Original image', image)
+            #cv2.imshow('Gray boi', gray)
+            face = faceCascade.detectMultiScale( imageGray, 1.25, 6)
+            for f in face:
+                x, y, w, h = [v for v in f ]
+                cv2.rectangle(imageCopy, (x,y), (x+w, y+h), (255,0,0), 3)
+                #croppen van gezicht
+                faceCrop = imageGray[y:y+h, x:x+w]
+            #resizen
+            dim = (64, 64)
+            resized = cv2.resize(faceCrop, dim, interpolation = methods[i])
 
-        #opslaan van de afbeeldingen
-        resizedName = pathResults + file + "Gray_resized.png"
-        cv2.imwrite(resizedName, resized) 
+            #opslaan van de afbeeldingen
+            resizedName = pathResults + file + "Gray_resized.png"
+            #cv2.imwrite(resizedName, resized) 
 
-        tEnd = time.time()
+            tEnd = time.time()
 
-        elapsedTime =  tEnd-tStart
-        resultsList.append(elapsedTime)
-        print(elapsedTime)
-        results.write( file + ': ' + str(elapsedTime) + '\n' )
+            elapsedTime =  tEnd-tStart
+            resultsList.append(elapsedTime)
+            print(elapsedTime)
+            results.write( file + ': ' + str(elapsedTime) + '\n' )
 
-    #berekenen en schrijven van average
-    average = sum(resultsList) / len(resultsList)
-    results.write("Average: " + str(average) + "\n" + "----------------------\n")
-    total.write( names[i] + " " + str(average) + '\n' ) 
-total.write("----------------------\n")
+        #berekenen en schrijven van average
+        average = sum(resultsList) / len(resultsList)
+        results.write("Average: " + str(average) + "\n" + "----------------------\n")
+        total.write( names[i] + " " + str(average) + '\n' ) 
+    total.write("----------------------\n")
