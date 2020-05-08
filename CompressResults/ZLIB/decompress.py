d25f376b30166db62c55e580e18b8c5e43cf3e09
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-05-08 11:24:27 +0200

    eerste foto's, code heeft nog errors omdat de py en folders ook worden gedecompressed. dat kan niet

--- CompressResults/ZLIB/decompress.py
+++ CompressResults/ZLIB/decompress.py
@@ -1,11 +1,11 @@
 import zlib
+import os
 
-path = "/home/pi/Opencv/CompressResults/ZLIB"
-
+imagePath = "/home/pi/Opencv/CompressResults/ZLIB/"
+pathUncom = "/home/pi/Opencv/CompressResults/ZLIB/Results/"
 for file in os.listdir(imagePath):
     print( file )
-    str_object1 = open(path+file, 'rb').read()
+    str_object1 = open(imagePath+file, 'rb').read()
     str_object2 = zlib.decompress(str_object1)
-    f = open(file+"uncompressed.png", 'wb')
+    f = open(pathUncom + file+"uncompressed.png", 'wb')
     f.write(str_object2)
-    f.close()
