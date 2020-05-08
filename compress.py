da183aaeca02ca4efc726737e76519a8cb26487e
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-05-08 10:25:05 +0200

    eerste opzet zlib

--- compress.py
+++ compress.py
@@ -1,7 +1,10 @@
 import time
 import os
+import fstream
 import cv2
 import gzip
+import brotli
+import zlib
 
 imagePath = "/home/pi/Opencv/CropResults/"
 resultPath = "/home/pi/Opencv/CompressResults/"
@@ -10,8 +13,18 @@ for file in os.listdir(imagePath):
     print( file )
     image = cv2.imread( imagePath + file )
     binaryImage = cv2.imencode('.png', image)[1].tostring()
-    with gzip.open( resultPath+file+".gz" , "wb" ) as result:
+    with gzip.open( resultPath+"/GZIP"+file+".gz" , "wb" ) as result:
         result.write(binaryImage)
+
+for file in os.listdir(imagePath):
+    print( file )
+    image = cv2.imread( imagePath + file )
+    binaryImage = cv2.imencode('.png', image)[1].tostring()
+    compressed = zlib.compress(binaryImage, 9)
+    ofstream outputFile
+    outputFile.open(resultPath+"/ZLIB"+file+".ZL")
+    outputFile << compressed
+    outputFile.close()
 
 
 #unzip with:
