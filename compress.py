d2eac19eb90bf911835b1a78f36025a1a0f4bd56
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-05-08 09:50:53 +0200

    eerts error loze run, push voor resultaten

--- compress.py
+++ compress.py
@@ -9,11 +9,11 @@ resultPath = "/home/pi/Opencv/CompressResults/"
 for file in os.listdir(imagePath):
     print( file )
     image = cv2.imread( imagePath + file )
-    binaryImage = cv2.imencode('.png', img)[1].tostring()
+    binaryImage = cv2.imencode('.png', image)[1].tostring()
     with gzip.open( resultPath+file+".gz" , "wb" ) as result:
         result.write(binaryImage)
 
 
 #unzip with:
->>> nparr = np.fromstring(STRING_FROM_DATABASE, np.uint8)
->>> img = cv2.imdecode(nparr, cv2.CV_LOAD_IMAGE_COLOR)
+#nparr = np.fromstring(STRING_FROM_DATABASE, np.uint8)
+#img = cv2.imdecode(nparr, cv2.CV_LOAD_IMAGE_COLOR)
