26e357e2c1ea43c434cbee1967550d621bbebb31
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-05-08 09:47:23 +0200

    compressie gzip errors

--- compress.py
+++ compress.py
@@ -9,12 +9,11 @@ resultPath = "/home/pi/Opencv/CompressResults/"
 for file in os.listdir(imagePath):
     print( file )
     image = cv2.imread( imagePath + file )
-    binaryImage = cv2.imencode( "png", image )
+    binaryImage = cv2.imencode('.png', img)[1].tostring()
     with gzip.open( resultPath+file+".gz" , "wb" ) as result:
         result.write(binaryImage)
 
 
-# >>> import gzip
-# >>> data = b'Python - Batteries included'
-# >>> with gzip.open("test.txt.gz", "wb") as f:
-# 	f.write(data)
+#unzip with:
+>>> nparr = np.fromstring(STRING_FROM_DATABASE, np.uint8)
+>>> img = cv2.imdecode(nparr, cv2.CV_LOAD_IMAGE_COLOR)
