bb4dafcd3ff925984de92d1c208c42c6ec0c4af6
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-05-11 14:16:36 +0200

    tijdstest toegevoegd

--- compress.py
+++ compress.py
@@ -7,13 +7,16 @@ import zlib
 imagePath = "/home/pi/Opencv/CropResults/"
 resultPath = "/home/pi/Opencv/CompressResults/"
 
+startGzip = time.time()
 for file in os.listdir(imagePath):
     print( file )
     image = cv2.imread( imagePath + file )
     binaryImage = cv2.imencode('.png', image)[1].tostring()
     with gzip.open( resultPath+"/GZIP/"+file+".gz" , "wb" ) as result:
         result.write(binaryImage)
+endGzip = time.time()
 
+startZlib = time.time()
 for file in os.listdir(imagePath):
     print( file )
     image = cv2.imread( imagePath + file )
@@ -21,11 +24,20 @@ for file in os.listdir(imagePath):
     compressed = zlib.compress(binaryImage, 9)
     outputfile = open(resultPath+"/ZLIB/Results/"+file+".txt", "w")
     outputfile.write(compressed)
+endZlib = time.time()
 
+startCV = time.time()
 for file in os.listdir(imagePath):
     print( file )
     image = cv2.imread( imagePath + file )
     compressed = cv2.imwrite(resultPath+"/CV/"+file+'.png', image,  [cv2.IMWRITE_PNG_COMPRESSION, 9])
+endCV = time.time()
+
+results = open(/home/pi/Opencv/CompressResults/+"times.txt", "a")
+result.write( "GZIP average: " + str((endGzip-startGzip)/amountOfPictures))
+result.write( "ZLIB average: " + str((endZlib startZlib)/amountOfPictures))
+result.write( "GZIP average: " + str((endCV-startCV)/amountOfPictures))
+result.close()
 #unzip with:
 #nparr = np.fromstring(STRING_FROM_DATABASE, np.uint8)
 #img = cv2.imdecode(nparr, cv2.CV_LOAD_IMAGE_COLOR)
